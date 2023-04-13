9ff083fa15da8d59f510133ee7f5e7e88d6282d4
sugarvoid <gamedev@kitargo.com>
2023-04-12 19:16:15 -0500

    Set up main gameplay loop

--- main.lua
+++ main.lua
@@ -1,6 +1,8 @@
 --main.lua
 
-require "scripts.load_media"
+require("scripts.load_media")
+
+
 
 local font = love.graphics.newFont("font/Blazma-Regular.ttf", 64)
 local text = ""
@@ -8,7 +10,9 @@ local debug =""
 local utf8 = require("utf8")
 local word_history
 local lettersToIgnore = {'x', 'q', 'u', 'z', 'w'}
-
+local sounds
+local score = 104
+local negative_multipler = 1
 
 
 local screenWidth, screenHeight = love.graphics.getDimensions()
@@ -20,7 +24,7 @@ function love.load()
     font:setFilter("nearest")
     love.graphics.setFont(font)
     love.keyboard.setKeyRepeat(true)
-    love.mouse.setVisible(false)
+    -- love.mouse.setVisible(false)
     sounds = load_sounds()
 end
 
@@ -39,25 +43,21 @@ function love.keypressed(key)
         local byteoffset = utf8.offset(text, -1)
 
         if byteoffset then
-            if string.len(text) > 1 
-                then
+            if string.len(text) > 1 then
                 -- remove the last UTF-8 character.
                 -- string.sub operates on bytes rather than UTF-8 characters, so we couldn't do string.sub(text, 1, -2).
                 text = string.sub(text, 1, byteoffset - 1)
                 playSound(sounds.erase)
-                end
+            end
         end
     end
+
     if key == "return" and text ~= "" then
-        debug = "enter func"
-        --play ding sound
-        playSound(sounds.correct)
-        add_word_to_list(text)
+        checkWord(text)
         
     end
-    if
-        key == "escape" then
-            love.event.quit()
+    if key == "escape" then
+        love.event.quit()
     end
 end
 
@@ -68,30 +68,28 @@ end
 
 
 function love.draw()
-    love.graphics.print(debug, 10, 10)
+    love.graphics.print(debug, 10, 0)
+    love.graphics.print(score, 10, 52)
     --love.graphics.setFont(font)
     --love.graphics.print("Hello set font", 20, 20)
     love.graphics.printf(text, 0, screenHeight / 2 - font:getHeight() / 2, screenWidth, "center")
 end
 
-function add_word_to_list(word)
-    debug = word
+
+function word_was_good(word)
+    -- add word to list
+    score = score + 1
     table.insert(word_history, word)
-    clear_current_word()
+    text = string.sub(text, -1)
 end
 
-function clear_current_word()
-    text = string.sub(text, -1)
-end 
 
-
-local function has_value (tab, val)
+function has_value (tab, val)
     for index, value in ipairs(tab) do
         if value == val then
             return true
         end
     end
-
     return false
 end
 
@@ -104,4 +102,42 @@ end
 function playSound(sound)
     love.audio.stop(sound)
     love.audio.play(sound)
+end
+
+
+function checkWord(word)
+    local inList
+
+    for line in love.filesystem.lines("data/wordlist.txt") do
+        if line == word then
+            inList = true
+            break
+        end
+    end
+
+    if inList == true then
+        -- Word good
+        playSound(sounds.correct)
+        word_was_good(word)
+    else
+        -- Word was bad
+        playSound(sounds.invalid)
+        score = clamp(0, (score - (1 * negative_multipler)), 900)
+        negative_multipler = negative_multipler + 1
+        text = ""
+    end
+end
+
+function saveWordsToTxt()
+    return
+end
+
+---
+-- Clamps a value to a certain range.
+-- @param min - The minimum value.
+-- @param val - The value to clamp.
+-- @param max - The maximum value.
+--
+function clamp(min, val, max)
+    return math.max(min, math.min(val, max));
 end
--- scripts/load_media.lua
+++ scripts/load_media.lua
@@ -4,7 +4,7 @@ function load_sounds()
   local Sounds = {}
   Sounds.click = love.audio.newSource("sound/click.wav", "static")
   Sounds.correct = love.audio.newSource("sound/correct.wav", "static")
-  Sounds.invaild = love.audio.newSource("sound/invaild.wav", "static")
+  Sounds.invalid = love.audio.newSource("sound/invalid.wav", "static")
   Sounds.level_up = love.audio.newSource("sound/level_up.wav", "static")
   Sounds.erase = love.audio.newSource("sound/erase.wav", "static")
   --local cursor_s = love.graphics.newImage("Media/Image/cursor.png")
